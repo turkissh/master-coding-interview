eecdaa3545412fb2b3e7a008165d7e60c57a138d
Emiliano Di Pierro <emiliano.dipierro@etermax.com>
2020-12-15 22:16:28 -0300

    DS LinkedList - Doubly Linked List

---
+++ src/main/kotlin/linkedLists/DoublyLinkedList.kt
@@ -0,0 +1,90 @@
+package linkedLists
+
+class DoublyLinkedList(value: Any) {
+	private var head: DoublyLinkedNode
+	private var tail: DoublyLinkedNode
+	private var length: Int
+
+	init {
+		this.head = DoublyLinkedNode(value, null, null)
+		this.tail = head
+		length = 1
+	}
+
+	fun append(newValue: Any) {
+		val newNode = DoublyLinkedNode(newValue, null, null)
+		appendValue(newNode)
+		incrementLength()
+	}
+
+	fun prepend(newValue: Any) {
+		val newHead = DoublyLinkedNode(newValue, null, this.head)
+		this.head.setLastNode(newHead)
+		this.head = newHead
+		incrementLength()
+	}
+
+	fun insert(index: Int, value: Any) {
+		var leaderNode = getLeaderNode(index)
+		val newNode = DoublyLinkedNode(value, leaderNode, leaderNode.next)
+		leaderNode.next?.setLastNode(newNode)
+		leaderNode.setNextNode(newNode)
+		incrementLength()
+	}
+
+	private fun getLeaderNode(index: Int): DoublyLinkedNode {
+		var currentIndex = 0
+		var current = this.head
+		while (current.next != null && currentIndex + 1 < index) {
+			current = current.next!!
+			currentIndex++
+		}
+		return current
+	}
+
+	fun remove(index: Int) {
+		val leaderNode = getLeaderNode(index)
+		val valueToRemove = leaderNode.next ?: return
+
+		leaderNode.setNextNode(valueToRemove.next)
+
+		decrementLength()
+	}
+
+	fun returnAll(): List<Any> {
+		val list = mutableListOf<Any>()
+		var node: DoublyLinkedNode? = this.head
+		while (node != null) {
+			list.add(node.value)
+			node = node.next
+		}
+
+		return list
+	}
+
+	private fun appendValue(newNode: DoublyLinkedNode) {
+		this.tail.setNextNode(newNode)
+		newNode.setLastNode(tail)
+		this.tail = newNode
+	}
+
+	fun length() = length
+
+	private fun incrementLength() {
+		length++
+	}
+
+	private fun decrementLength() {
+		length--
+	}
+}
+
+class DoublyLinkedNode(val value: Any, var last: DoublyLinkedNode?, var next: DoublyLinkedNode?) {
+	fun setLastNode(node: DoublyLinkedNode?) {
+		this.last = node
+	}
+
+	fun setNextNode(node: DoublyLinkedNode?) {
+		this.next = node
+	}
+}
---
+++ src/test/kotlin/linkedLists/DoublyLinkedListTest.kt
@@ -0,0 +1,138 @@
+package linkedLists
+
+import org.assertj.core.api.Assertions.assertThat
+import org.junit.jupiter.api.Test
+
+class DoublyLinkedListTest {
+
+	@Test
+	internal fun `Save one value`() {
+		val customLinkedList = DoublyLinkedList(5)
+
+		val values = customLinkedList.returnAll()
+
+		assertThat(values).containsExactlyInAnyOrder(5)
+	}
+
+	@Test
+	internal fun `Append value`() {
+		val customLinkedList = DoublyLinkedList(5)
+
+		customLinkedList.append(20)
+
+		val values = customLinkedList.returnAll()
+		assertThat(values).containsExactlyInAnyOrder(5, 20)
+		assertThat(customLinkedList.length()).isEqualTo(2)
+	}
+
+	@Test
+	internal fun `Append two value`() {
+		val customLinkedList = DoublyLinkedList(5)
+		customLinkedList.append(20)
+
+		customLinkedList.append(55)
+
+		val values = customLinkedList.returnAll()
+		assertThat(values).containsExactlyInAnyOrder(5, 20, 55)
+		assertThat(customLinkedList.length()).isEqualTo(3)
+	}
+
+	@Test
+	internal fun `Prepend value`() {
+		val customLinkedList = DoublyLinkedList(5)
+
+		customLinkedList.prepend(20)
+
+		val values = customLinkedList.returnAll()
+		assertThat(values).containsExactlyInAnyOrder(20,5)
+		assertThat(customLinkedList.length()).isEqualTo(2)
+	}
+
+	@Test
+	internal fun `Prepend two value`() {
+		val customLinkedList = DoublyLinkedList(5)
+		customLinkedList.prepend(20)
+
+		customLinkedList.prepend(65)
+
+		val values = customLinkedList.returnAll()
+		assertThat(values).containsExactlyInAnyOrder(65,20,5)
+		assertThat(customLinkedList.length()).isEqualTo(3)
+	}
+
+	@Test
+	internal fun `insert value`() {
+		val customLinkedList = DoublyLinkedList(5)
+		customLinkedList.prepend(20)
+		customLinkedList.prepend(65)
+
+		customLinkedList.insert(1, 30)
+
+		val values = customLinkedList.returnAll()
+		assertThat(values).containsExactlyInAnyOrder(65,30,20,5)
+		assertThat(customLinkedList.length()).isEqualTo(4)
+	}
+
+	@Test
+	internal fun `insert value on head`() {
+		val customLinkedList = DoublyLinkedList(5)
+		customLinkedList.prepend(20)
+		customLinkedList.prepend(65)
+
+		customLinkedList.insert(0, 30)
+
+		val values = customLinkedList.returnAll()
+		assertThat(values).containsExactlyInAnyOrder(30,65,20,5)
+		assertThat(customLinkedList.length()).isEqualTo(4)
+	}
+
+	@Test
+	internal fun `insert value far from length`() {
+		val customLinkedList = DoublyLinkedList(5)
+
+		customLinkedList.insert(5, 30)
+
+		val values = customLinkedList.returnAll()
+		assertThat(values).containsExactlyInAnyOrder(5,30)
+		assertThat(customLinkedList.length()).isEqualTo(2)
+	}
+
+	@Test
+	internal fun `remove value`() {
+		val customLinkedList = DoublyLinkedList(5)
+		customLinkedList.prepend(20)
+		customLinkedList.prepend(65)
+
+		customLinkedList.remove(1)
+
+		val values = customLinkedList.returnAll()
+		assertThat(values).containsExactlyInAnyOrder(65,5)
+		assertThat(customLinkedList.length()).isEqualTo(2)
+	}
+
+	@Test
+	internal fun `remove last value`() {
+		val customLinkedList = DoublyLinkedList(5)
+		customLinkedList.prepend(20)
+		customLinkedList.prepend(65)
+
+		customLinkedList.remove(2)
+
+		val values = customLinkedList.returnAll()
+		assertThat(values).containsExactlyInAnyOrder(65,20)
+		assertThat(customLinkedList.length()).isEqualTo(2)
+	}
+
+	@Test
+	internal fun `remove pass last value`() {
+		val customLinkedList = DoublyLinkedList(5)
+		customLinkedList.prepend(20)
+		customLinkedList.prepend(65)
+
+		customLinkedList.remove(6)
+
+		val values = customLinkedList.returnAll()
+		assertThat(values).containsExactlyInAnyOrder(65,20,5)
+		assertThat(customLinkedList.length()).isEqualTo(3)
+	}
+}
