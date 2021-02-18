af42ae47628b91afe91781fc355cc0590926e2dc
Emiliano Di Pierro <emiliano.dipierro@etermax.com>
2021-02-18 16:32:11 -0300

    Algo Sorting - Questions

---
+++ src/main/kotlin/sorting/SortingQuestions.kt
@@ -0,0 +1,21 @@
+package sorting
+
+class SortingQuestions {
+
+	//#1 - Sort 10 schools around your house by distance: Insertion
+
+	//#2 - eBay sorts listings by the current Bid amount: Merge / Could be Radix-Counting
+
+	//#3 - Sport scores on ESPN: Quick
+
+	//#4 - Massive database (can't fit all into memory) needs to sort through past year's user data: Merge (not in memory)
+
+	//#5 - Almost sorted Udemy review data needs to update and add 2 new reviews: Insertion
+
+	//#6 - Temperature Records for the past 50 years in Canada: Quick
+
+	//#7 - Large user name database needs to be sorted. Data is very random.: QuickSort
+
+	//#8 - You want to teach sorting for the first time: Bubble/Selection
+
+}
